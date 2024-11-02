9a082f0ad297c012c2fd00d3bd28a1745fbf8e9c
rubemar jr <rubemarrocha22@gmail.com>
2024-11-02 14:24:37 -0300

    fix database import in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import router from "./routes";
 import "dotenv/config";
-import "./database";
+import "./database/index";
 import { resolve } from "path";
 class App {
     constructor() {
