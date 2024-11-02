f913847669dc8794a9a2fccad12c7ab416d503e3
rubemar jr <rubemarrocha22@gmail.com>
2024-11-02 15:03:38 -0300

    update module system with sucrase

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import router from "./routes";
 import "dotenv/config";
-import "./database/index";
+import "./database";
 import { resolve } from "path";
 class App {
     constructor() {
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from "./app";
 
 app.listen(3000, () => {
-  console.log("Server running on localhost:3000");
+    console.log("Server running on localhost:3000");
 });
