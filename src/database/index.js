d7459ccf6fff83238ea9c12315942a645d583bf8
rubemar jr <rubemarrocha22@gmail.com>
2024-11-02 20:15:47 -0300

    update sequelize connection

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ import User from "../app/models/User.js";
 import Product from "../app/models/Product.js";
 import Categories from "../app/models/Categories.js";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 const models = [User, Product, Categories];
 class Database {
@@ -13,7 +14,14 @@ class Database {
 
     init() {
         this.connection = new Sequelize(
-            "postgresql://postgres:NPFTxfnuJOfwEOtiShsMudLUHeYFjUzr@autorack.proxy.rlwy.net:58077/railway"
+            process.env.POSTGRES_DB,
+            process.env.POSTGRES_USER,
+            process.env.POSTGRES_PASSWORD,
+            {
+                host: process.env.POSTGRES_HOST,
+                port: process.env.POSTGRES_PORT,
+                dialect: "postgres"
+            }
         );
         models
             .map((model) => model.init(this.connection))
